95100b5bf83eabcb33ba770a40f1871ff73736cc
AdiSchiff <adidi.schiff@gmail.com>
2024-05-08 19:43:02 +0300

    rearanged the order of the routs

--- Server/routes/azrieli_store.js
+++ Server/routes/azrieli_store.js
@@ -5,13 +5,14 @@ var router = express.Router();
 
 router.route('/')
     .post(storeController.createNewStore);
+router.route('/:storeType')
+    .get(storeController.getStoresByType);
+    //find solution to the string problame
 router.route('/:storename')
     .get(storeController.getStoresByName)
     .delete(storeController.deleteStoreByName);
 router.route('/:floor')
     .get(storeController.getStoresByFloor)
     .put(storeController.updateFloor);
-router.route('/:storeType')
-    .get(storeController.getStoresByType);
 
 module.exports = router;
