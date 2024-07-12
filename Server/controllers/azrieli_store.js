0bc85e3e71e2085d17b21b9501f8a3b668aa32fd
AdiSchiff <adidi.schiff@gmail.com>
2024-07-12 11:20:58 +0300

    fixed the paging function

--- Server/controllers/azrieli_store.js
+++ Server/controllers/azrieli_store.js
@@ -132,24 +132,29 @@ const updateFloor = async (req, res) => {
   res.status(401).send();
 };
 
-const getStoresByType = async (req, res) => {
+const getStoresByTypePaged = async (req, res) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
-    if (loginController.isLoggedIn(token) !== -1) {
-      const storeType = req.params.storeType;
-      const mallName = req.query.mallname; // Use req.query.mallname for the query parameter
-      let stores;
-      if (storeType === "all") {
-        stores = await storeService.getAll();
-      } else {
-        // Get an array of all the stores from the given storeType
-        stores = await storeService.getStoresByType(storeType);
+      const token = req.headers.authorization.split(" ")[1];
+      if (!(await loginController.isLoggedIn(token))) {
+        return res.status(401).send("Unauthorized");
       }
-      if (!stores || stores.length === 0) {
-        return res
-          .status(404)
-          .send("No stores found for the given store type.");
-      }
+
+    const storeType = req.params.storeType;
+
+    const page = parseInt(req.query.page) || 0;
+
+    const limit = parseInt(req.query.limit) || 10;
+
+    let stores;
+    if (storeType === "all") {
+      stores = await storeService.getAll();
+    } else {
+      stores = await storeService.getStoresByType(storeType);
+    }
+    if (!stores || stores.length === 0) {
+      return res.status(404).send("No stores found for the given store type.");
+    }
+
       const azrieliStores = [];
       for (const store of stores) {
         // Check if the current store exists in the mall
@@ -172,16 +177,41 @@ const getStoresByType = async (req, res) => {
         // Add the store object to the mall's stores array
         azrieliStores.push(checkedStore);
       }
-      const category = {
-        categoryName: storeType,
-        storesList: azrieliStores,
-      };
-      return res.status(200).json(category);
-    }
-    res.status(401).send("Unauthorized");
+      const pagedStores = azrieliStores.slice(page * limit, (page + 1) * limit);
+      return res.status(200).json(pagedStores);
   } catch (error) {
-    console.error("Error in getStoresByType function: ", error);
+    console.error("Error in getStoresByTypePaged function: ", error);
     res.status(500).send("Internal Server Error");
+  }
+};
+
+const getStoresByType = async (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+  if (!(await loginController.isLoggedIn(token))) {
+    return res.status(401).send("Unauthorized");
+  }
+  const storeType = req.params.storeType;
+  const mallName = req.query.mallname; // Use req.query.mallname for the query parameter
+  let stores;
+  if (storeType === "all") {
+    stores = await storeService.getAll();
+  } else {
+    // Get an array of all the stores from the given storeType
+    stores = await storeService.getStoresByType(storeType);
+  }
+  if (!stores || stores.length === 0) {
+    return res.status(404).send("No stores found for the given store type.");
+  }
+  const azrieliStores = [];
+  for (const store of stores) {
+    // Check if the current store exists in the mall
+    const mallStores = await AztieliStoreService.getStoresByName(
+      store.storename,
+      mallName
+    );
+    if (!mallStores || mallStores.length === 0) {
+      continue;
+    }
   }
 };
 
@@ -220,6 +250,7 @@ const getTypes = async (req, res) => {
 };
 
 module.exports = {
+  getStoresByTypePaged,
   createNewStore,
   getStoresByName,
   getStoresByFloor,
